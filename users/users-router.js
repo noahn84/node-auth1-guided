0d5d52a45f711e3dc809561ffdd6f1eefbb208c3
Jason Maurer <jason@maur.co>
2020-09-01 19:36:55 -0600

    Code from guided lecture

--- users/users-router.js
+++ users/users-router.js
@@ -1,9 +1,11 @@
 const express = require("express")
+const bcrypt = require("bcryptjs")
 const Users = require("./users-model")
+const usersMiddleware = require("./users-middleware")
 
 const router = express.Router()
 
-router.get("/users", async (req, res, next) => {
+router.get("/users", usersMiddleware.restrict(), async (req, res, next) => {
 	try {
 		res.json(await Users.find())
 	} catch(err) {
@@ -24,7 +26,8 @@ router.post("/users", async (req, res, next) => {
 
 		const newUser = await Users.add({
 			username,
-			password,
+			// hash the password with a time complexity of "14"
+			password: await bcrypt.hash(password, 14),
 		})
 
 		res.status(201).json(newUser)
@@ -44,10 +47,38 @@ router.post("/login", async (req, res, next) => {
 			})
 		}
 
+		// compare the plain text password from the request body to the
+		// hash we have stored in the database. returns true/false.
+		const passwordValid = await bcrypt.compare(password, user.password)
+
+		// check if hash of request body password matches the hash we already have
+		if (!passwordValid) {
+			return res.status(401).json({
+				message: "Invalid Credentials",
+			})
+		}
+
+		// create a new session for the user
+		req.session.user = user
+
 		res.json({
 			message: `Welcome ${user.username}!`,
 		})
 	} catch(err) {
+		next(err)
+	}
+})
+
+router.get("/logout", usersMiddleware.restrict(), async (req, res, next) => {
+	try {
+		req.session.destroy((err) => {
+			if (err) {
+				next(err)
+			} else {
+				res.status(204).end()
+			}
+		})
+	} catch (err) {
 		next(err)
 	}
 })
