2cd2e00600e7ef68f42c11f09d2b2d7331b43e4a
Jason Maurer <jason@maur.co>
2020-10-27 12:19:08 -0600

    Add users middleware file

---
+++ users/users-middleware.js
@@ -0,0 +1,8 @@
+function restrict() {
+	// Create a middleware function that restricts routes to authorized users only.
+	// It should get the username and password from the request headers.
+}
+
+module.exports = {
+	restrict,
+}
